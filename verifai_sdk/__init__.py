2c7bea72e25251ae993ec608905e853c5b9f8aa1
Joshua Peper <josh@peperzaken.nl>
2018-09-04 17:20:08 +0200

    PDF and Bitmap support

--- verifai_sdk/__init__.py
+++ verifai_sdk/__init__.py
@@ -2,3 +2,4 @@ from verifai_sdk.version import VERSION
 from verifai_sdk.service import VerifaiService
 from verifai_sdk.document import VerifaiDocument, VerifaiDocumentMrz, \
     VerifaiDocumentZone
+from verifai_sdk.pdf_support import VerifaiPdfProcessor
